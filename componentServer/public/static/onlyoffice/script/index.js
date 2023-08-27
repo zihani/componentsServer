28bd3c9f4193a4289cb16bf70e06a4086b810f9f
pczihan <lzzihan@email.cn>
2023-08-27 13:33:53 +0800

    edit

--- componentServer/public/static/onlyoffice/script/index.js
+++ componentServer/public/static/onlyoffice/script/index.js
@@ -9,37 +9,28 @@ const GetQueryString = function(name) {
 const createScript = (text)=>{
         switch(text)
             {
-                case "tinymce":
-                    return new Promise(function (resolve, reject) {
-                        var s = document.createElement('script')
-                        s.setAttribute('type', "text/javascript")
-                        s.setAttribute('src', window.location.origin + "/static/tinymce/tinymce.js")
-                        document.head.appendChild(s)
-                        s.onload = resolve
-                        s.onerror = reject
-                    })
                 case "onlyoffice":
                     return new Promise(function (resolve, reject) {
                         var s = document.createElement('script')
                         s.setAttribute('type', "text/javascript")
                          // uat 
-                        let _url = localStorage.getItem('_url')
-                        let uat = _url.indexOf("uat")
-                        let dev = _url.indexOf("test")
-                        let y = _url.indexOf("y-idea")
-                        // debugger;
-                        if(uat !== -1){
-                            s.setAttribute('src', "https://ixxx/web-apps/apps/api/documents/api.js")
-                        }
-                        if(dev !== -1){         
-                            s.setAttribute('src',"https://ixxx/web-apps/apps/api/documents/api.js")
-                        }
-                        if(y !== -1){
-                            s.setAttribute('src', "http://ixxx/web-apps/apps/api/documents/api.js")
-                        }
-                        if(dev === -1 && uat === -1 && y === -1){
-                            s.setAttribute('src',"https://ixxx/web-apps/apps/api/documents/api.js")
-                        }
+                        // let _url = localStorage.getItem('_url')
+                        // let uat = _url.indexOf("uat")
+                        // let dev = _url.indexOf("test")
+                        // let y = _url.indexOf("y-idea")
+                        // // debugger;
+                        // if(uat !== -1){
+                        //     s.setAttribute('src', "https://ixxx/web-apps/apps/api/documents/api.js")
+                        // }
+                        // if(dev !== -1){         
+                        //     s.setAttribute('src',"https://ixxx/web-apps/apps/api/documents/api.js")
+                        // }
+                        // if(y !== -1){
+                        //     s.setAttribute('src', "http://ixxx/web-apps/apps/api/documents/api.js")
+                        // }
+                        // if(dev === -1 && uat === -1 && y === -1){
+                        //     s.setAttribute('src',"https://ixxx/web-apps/apps/api/documents/api.js")
+                        // }
                         s.setAttribute('src',"http://192.168.127.132/web-apps/apps/api/documents/api.js")
                         document.head.appendChild(s)
                         s.onload = resolve
