455a24ae3cbb18cde34fc2ab95e9d9295c3d3ea1
pczihan <lzzihan@email.cn>
2023-09-23 16:05:14 +0800

    删除gblog

--- gblog/src/api/article/index.js
+++
@@ -1,9 +0,0 @@
-import request from '@/utils/request'
-
-export function articleList(params) {
-    return request({
-        url: '/post/list',
-        method: 'get',
-        params: params
-    })
-}
